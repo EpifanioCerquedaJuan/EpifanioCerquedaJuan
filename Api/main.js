f942792532a2ac90f73cd4f2486f060dd2e458b4
EpifanioCJ <161562051+EpifanioCerquedaJuan@users.noreply.github.com>
2024-07-03 19:36:24 -0600

    Add files via upload

---
+++ Api/main.js
@@ -0,0 +1,31 @@
+fetch('https://hp-api.onrender.com/api/characters')
+ .then(respuesta => {
+    return respuesta.json();
+   
+ })
+
+ .then((respuesta) =>
+    {
+    const data = respuesta;
+        personajes(data);
+console.log(respuesta)
+function personajes(respuesta) {
+    const contenido = document.querySelector('.contenido')
+    //console.log(respuesta)
+    const { value } = respuesta;
+    //console.log(value)
+    const html = 
+    `
+    <section>
+    <p>${value}</p>
+    
+    </section>
+    
+    `;
+    contenido.innerHTML += html;
+}
+    })
+    .catch(error =>{
+        const modalError = `<h1>${error.message}</h1>`
+        document.DOCUMENT_TYPE_NODE.innerHTML = modalError;
+    });
